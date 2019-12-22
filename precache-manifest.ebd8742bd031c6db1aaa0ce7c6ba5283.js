8d8a3abba5f07c417c96d7a2241a1db932f0fede
sujatasule <46859953+sujatasule@users.noreply.github.com>
2019-12-22 17:51:40 +0200

    Updates

---
+++ precache-manifest.ebd8742bd031c6db1aaa0ce7c6ba5283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5457e077900eb69b74316cdc9793db48",
+    "url": "/matrieal-ui/index.html"
+  },
+  {
+    "revision": "c6b3cc053fc3e40ded0e",
+    "url": "/matrieal-ui/static/css/main.1ac4d896.chunk.css"
+  },
+  {
+    "revision": "ff904d64b4a1cb8a8a6f",
+    "url": "/matrieal-ui/static/js/2.d687af97.chunk.js"
+  },
+  {
+    "revision": "c6b3cc053fc3e40ded0e",
+    "url": "/matrieal-ui/static/js/main.bf2dd6b1.chunk.js"
+  },
+  {
+    "revision": "2d1d4a0e15c7dbd203d1",
+    "url": "/matrieal-ui/static/js/runtime-main.3a96f69a.js"
+  }
+]);
